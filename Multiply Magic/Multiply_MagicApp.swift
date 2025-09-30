1fb2e04c1582b0c2e8e06c0458f1e2fe6e2a1d41
josekameydev01 <gk01cracklitos@gmail.com>
2025-09-29 22:05:23 -0600

    first commit

---
+++ Multiply Magic/Multiply_MagicApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multiply_MagicApp.swift
+//  Multiply Magic
+//
+//  Created by carlosgalvankamey on 9/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Multiply_MagicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
