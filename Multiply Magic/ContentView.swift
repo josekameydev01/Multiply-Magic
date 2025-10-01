c7eb9eb78b1ceba409e65ec9dd7b021bc44ef043
josekameydev01 <gk01cracklitos@gmail.com>
2025-09-30 22:39:07 -0600

    subtitle/instructions created

--- Multiply Magic/ContentView.swift
+++ Multiply Magic/ContentView.swift
@@ -19,13 +19,21 @@ struct ContentView: View {
                 endPoint: .bottom
             )
             
-            Text("Multiply Magic")
-                .font(.largeTitle.bold())
-                .foregroundStyle(.white)
-                .frame(width: 300, height: 70)
-                .background(Color(red: 0.94, green: 0.45, blue: 0.20))
-                .cornerRadius(20)
-            
+            VStack {
+                Text("Multiply Magic")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    .fontWeight(.bold)
+                    .frame(width: 300, height: 70)
+                    .background(Color(red: 0.94, green: 0.45, blue: 0.20))
+                    .cornerRadius(20)
+                
+                Text("Which tables do you want to practice?")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(Color(red: 0.2, green: 0.2, blue: 0.35))
+                    
+            }
         }
         .ignoresSafeArea()
     }
